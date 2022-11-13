07454f92df489be19ebd901d91ffc85e20edd38b
北沢亮和 <ryowa.kitazawa@gmail.com>
2022-11-13 22:38:28 +0900

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import firebase from "firebase";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+  REACT_APP_MEASUREMENT_ID,
+} from "react-native-dotenv";
 
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: REACT_APP_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAIN,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+    appId: REACT_APP_APP_ID,
+    measurementId: REACT_APP_MEASUREMENT_ID,
+  };
+  firebase.initializeApp(firebaseConfig);
+}
+
+type Shop = {
+  name: string;
+  place: string;
+};
 export default function App() {
+  const [shops, setShops] = useState<Shop[]>([]);
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+
+  const getFirebaseItems = async () => {
+    const snapshot = await firebase.firestore().collection("shops").get();
+    const shops = snapshot.docs.map((doc) => doc.data() as Shop);
+    setShops(shops);
+  };
+
+  const shopItems = shops.map((shop, index) => (
+    <View style={{ margin: 10 }} key={index.toString()}>
+      <Text>{shop.name}</Text>
+      <Text>{shop.place}</Text>
+    </View>
+  ));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {/* <Text>Hello Review App!</Text> */}
+      {shopItems}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +60,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

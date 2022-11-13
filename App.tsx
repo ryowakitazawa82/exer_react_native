26bac747765d7e9084041e3af7316fb3db69ce2a
北沢亮和 <ryowa.kitazawa@gmail.com>
2022-11-13 23:21:29 +0900

    設定ファイル編集とか

--- App.tsx
+++ App.tsx
@@ -2,33 +2,9 @@ import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
 import firebase from "firebase";
 import { StyleSheet, Text, View } from "react-native";
-import {
-  REACT_APP_API_KEY,
-  REACT_APP_AUTH_DOMAIN,
-  REACT_APP_PROJECT_ID,
-  REACT_APP_STORAGE_BUCKET,
-  REACT_APP_MESSAGING_SENDER_ID,
-  REACT_APP_APP_ID,
-  REACT_APP_MEASUREMENT_ID,
-} from "react-native-dotenv";
+import { getShops } from "./src/lib/firebase";
+import { Shop } from "./types/shops";
 
-if (!firebase.apps.length) {
-  const firebaseConfig = {
-    apiKey: REACT_APP_API_KEY,
-    authDomain: REACT_APP_AUTH_DOMAIN,
-    projectId: REACT_APP_PROJECT_ID,
-    storageBucket: REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-    appId: REACT_APP_APP_ID,
-    measurementId: REACT_APP_MEASUREMENT_ID,
-  };
-  firebase.initializeApp(firebaseConfig);
-}
-
-type Shop = {
-  name: string;
-  place: string;
-};
 export default function App() {
   const [shops, setShops] = useState<Shop[]>([]);
   useEffect(() => {
@@ -37,7 +13,7 @@ export default function App() {
 
   const getFirebaseItems = async () => {
     const snapshot = await firebase.firestore().collection("shops").get();
-    const shops = snapshot.docs.map((doc) => doc.data() as Shop);
+    const shops = await getShops();
     setShops(shops);
   };
 
@@ -50,7 +26,6 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {/* <Text>Hello Review App!</Text> */}
       {shopItems}
       <StatusBar style="auto" />
     </View>
